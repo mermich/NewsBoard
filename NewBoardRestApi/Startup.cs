280cfc112ba6b96c0a597fd9632c014a0639f6c4
etienne mermillod <etienne.mermillod@tks.ca>
2017-12-08 11:16:43 -0400

    reverted back user.

--- NewBoardRestApi/Startup.cs
+++ NewBoardRestApi/Startup.cs
@@ -21,6 +21,7 @@ namespace NewBoardRestApi
             Configuration = builder.Build();
         }
 
+
         public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
