e72328661f3d03c044f10fd15ef8b2afb1d653fc
etienne mermillod <etienne_mermillod@hotmail.com>
2018-04-05 10:06:02 -0300

    finder score update

--- ApiTools/SyndicationSearch/FeedFinderStrategy.cs
+++ ApiTools/SyndicationSearch/FeedFinderStrategy.cs
@@ -8,32 +8,19 @@ namespace ApiTools.SyndicationSearch
     public class SyndicationSearchStrategy
     {
         List<ASyndicationSearch> searchers = new List<ASyndicationSearch>();
-        ASyndicationSearch defaultSyndicationSearch;
 
-        public SyndicationSearchStrategy(HtmlDocumentPageWrapper doc) : this(new DefaultSyndicationSearch(doc), new List<ASyndicationSearch> { new WordPressSyndicationSearch(doc), new YoutubeSyndicationSearch(doc), new WordPressElegantThemesSyndicationSearch(doc), new LefigaroSyndicationSearch(doc) })
+        public SyndicationSearchStrategy(HtmlDocumentPageWrapper doc) : this(new List<ASyndicationSearch> { new DefaultSyndicationSearch(doc), new WordPressSyndicationSearch(doc), new YoutubeSyndicationSearch(doc), new WordPressElegantThemesSyndicationSearch(doc), new LefigaroSyndicationSearch(doc) })
         {
         }
 
-        public SyndicationSearchStrategy(ASyndicationSearch defaultSyndicationSearch, IList<ASyndicationSearch> searchers)
+        public SyndicationSearchStrategy(IList<ASyndicationSearch> searchers)
         {
-            this.defaultSyndicationSearch = defaultSyndicationSearch;
             this.searchers.AddRange(searchers);
         }
 
         public ASyndicationSearch GetFeedSearch()
         {
             return searchers.OrderBy(f => f.MatchScore()).LastOrDefault();
-        }
-
-        public ASyndicationSearch GetFeedSearchOrDefault()
-        {
-            var finder = searchers.OrderBy(f => f.MatchScore()).LastOrDefault();
-            if (finder.MatchScore() == 0)
-            {
-                finder = defaultSyndicationSearch;
-            }
-
-            return finder;
         }
     }
 }
